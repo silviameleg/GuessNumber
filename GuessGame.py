38cd3895215cd795d89e978fe8a2056d719b9205
Silvia <silvia.meleg@gmail.com>
2020-08-15 15:12:01 +0100

    testing done and errors fixed

--- GuessGame.py
+++ GuessGame.py
@@ -1,32 +1,43 @@
 import random
 
-print('''\nGuess the Number is a fun game!
-      
-Here\'s how it works:
-I am thinking of an integer number between 0 and 10.
-You have to guess what that number is!
-You have three chances.
-Good luck!
-''')
+def play():
+    print('''\nGuess the Number is a fun game!
+        
+    Here\'s how it works:
+    I am thinking of an integer number between 0 and 10.
+    You have to guess what that number is!
+    You have three chances.
+    Good luck!
+    ''')
 
-#generate a random number
-luckyNum = random.randint(0,10)
-print(luckyNum)
+    #generate a random number
+    luckyNum = random.randint(0,10)
+    #print command is for testing purposes only
+    print(luckyNum)
 
-#get the three gueses
-num1 = int(input('Type your first guess here: '))
+    # must check the imput to be an integer between the range
+    permitedRange = range(11)
+    def checkImput(imp):
+        if imp not in permitedRange:
+            print('Please enter an integer number between 0 and 10.\n' + 
+            'Let\'s start again!\n')
+            play()
 
-if num1 == luckyNum:
-    print('You\'re the winner!!!')
-else:
-    num2 = int(input('Try again: '))
-    if num2 == luckyNum:
+    #get the three gueses
+    num1 = int(input('Type your first guess here: '))
+    checkImput(num1)
+    if num1 == luckyNum:
         print('You\'re the winner!!!')
     else:
-        num3 = int(input('One more chance to be the winner: '))
-        if num3 == luckyNum:
-            print('You win!!!')
+        num2 = int(input('Try again: '))
+        checkImput(num2)
+        if num2 == luckyNum:
+            print('You\'re the winner!!!')
         else:
-            print('Game over')
-
-# must check the imput to be an integer between the range
+            num3 = int(input('One more chance to be the winner: '))
+            checkImput(num3)
+            if num3 == luckyNum:
+                print('You\'re the winner!!!')
+            else:
+                print('Game over')
+play()
